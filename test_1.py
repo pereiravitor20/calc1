7e7ce2c40838bcbd24c916031684cd630991ecc1
pereiravitor20 <43051258+pereiravitor20@users.noreply.github.com>
2018-09-06 20:21:54 -0300

    Create test_1.py

---
+++ test_1.py
@@ -0,0 +1,18 @@
+import pytest
+
+from arquivo import soma, menos, this_num
+
+
+def test_mais():
+        assert soma(3,2) == 5
+        assert soma(3,3) == 6
+        assert soma(2.5,  1.5) == 4
+        assert soma( -3, -11) == -14
+
+
+def test_usandostring():
+        assert soma('3', '5') == 8
+        assert soma('xyz',1) == None
+
+def test_menos():
+assert menos(2,2) == 0
